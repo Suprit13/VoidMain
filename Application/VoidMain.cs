ac790ddfc5208a604a74824d57f4d099b1ef3870
DMace44 <akgamergaming444@gmail.com>
2023-12-06 19:35:15 +0530

    Added interface

---
+++ Application/VoidMain.cs
@@ -0,0 +1,15 @@
+// Main Program begins here...
+
+using System;
+using static System.Console;
+
+namespace VoidMain
+{
+    public sealed class VoidMain
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+        }
+    }
+}
