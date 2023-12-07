45bff4c6f2c1bfa2813f1e705433552c56677e67
DMace44 <akgamergaming444@gmail.com>
2023-12-07 15:01:49 +0530

    Implemented more debugging into the API

--- API/TestAPI.cs
+++ API/TestAPI.cs
@@ -6,16 +6,15 @@ namespace VoidMainAPI
 {
     public sealed class TestAPI
     {
-        private string builtCondition;
+        private readonly string builtCondition;
+        private readonly int builtCode;
 
         public TestAPI()
         {
             builtCondition = "API constructed successfully!";
+            builtCode = 0;
         }
 
-        public void PrintAPIState()
-        {
-            Console.WriteLine(builtCondition);
-        }
+        public void PrintAPIState() => Console.WriteLine($"{builtCondition}\nBuild Code: {builtCode}");
     }
 }
